f7a3a3c18dffa21915a189858d7a3a847ebadea4
ava drb <ava.darabi89@gmail.com>
2019-10-18 16:13:57 +0330

    add user auth

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="tabs is-boxed">
+        <ul>
+            <li class="is-active">
+                <a>
+                    <span class="icon is-small"><i class="fas fa-image" aria-hidden="true"></i></span>
+                    <span>Backlog</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <span class="icon is-small"><i class="fas fa-music" aria-hidden="true"></i></span>
+                    <span>Sprints</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <span class="icon is-small"><i class="fas fa-film" aria-hidden="true"></i></span>
+                    <span>Members</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <span class="icon is-small"><i class="far fa-file-alt" aria-hidden="true"></i></span>
+                    <span>Statistics</span>
+                </a>
+            </li>
+        </ul>
+        
+    </div>
+    <div>
+        rtyett
+    </div>
+                
+@endsection
